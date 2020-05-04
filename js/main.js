efc68c49bf8effc20f74078404f2244da64f6b8c
boosun13 <double0two22@gmail.com>
2020-05-04 17:01:31 +0900

    操作可能段階

---
+++ js/main.js
@@ -0,0 +1,38 @@
+"use script";
+{
+const timer = document.getElementById("timer");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+let startTime;
+let timeoutId;
+
+function countUp(){
+    // console.log(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2, "0");
+    const s = String(d.getSeconds()).padStart(2, "0");
+    const ms = String(d.getMilliseconds()).padStart(3, "0");
+    timer.textContent = `${m}:${s}.${ms}`;
+
+    timeoutId = setTimeout(() =>{
+        countUp();
+    }, 10);
+}
+
+start.addEventListener("click", () =>{
+    startTime = Date.now()
+    countUp();
+});
+
+stop.addEventListener("click", () =>{
+    clearTimeout(timeoutId);
+    // setTimeout のキャンセル
+});
+
+reset.addEventListener("click", () =>{
+    timer.textContent = "00:00.000";
+});
+
+}
