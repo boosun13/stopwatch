fc39fe0557d9c5cf20b3677556e7c45c54d56a8f
boosun13 <double0two22@gmail.com>
2020-05-04 17:21:24 +0900

    カウントの正しい動作

--- js/main.js
+++ js/main.js
@@ -7,10 +7,11 @@ const reset = document.getElementById("reset");
 
 let startTime;
 let timeoutId;
+let elapsedTime = 0;
 
 function countUp(){
     // console.log(Date.now() - startTime);
-    const d = new Date(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime + elapsedTime);
     const m = String(d.getMinutes()).padStart(2, "0");
     const s = String(d.getSeconds()).padStart(2, "0");
     const ms = String(d.getMilliseconds()).padStart(3, "0");
@@ -29,10 +30,14 @@ start.addEventListener("click", () =>{
 stop.addEventListener("click", () =>{
     clearTimeout(timeoutId);
     // setTimeout のキャンセル
+    elapsedTime += Date.now() - startTime;
+    //止めた時間を定数として記録。総数とするために+=としている。
 });
 
 reset.addEventListener("click", () =>{
     timer.textContent = "00:00.000";
+    elapsedTime = 0;
+    //定数を元にも戻す。
 });
 
 }
